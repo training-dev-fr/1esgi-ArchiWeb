f01347cea536bbfb4a141d61d04e62002457bdef
training-dev <contact@training-dev.fr>
2023-03-13 12:24:30 +0100

    retrait var dump

--- Controller/user.controller.php
+++ Controller/user.controller.php
@@ -24,9 +24,7 @@
 
     function updateUser($id)
     {
-        var_dump(file_get_contents("php://input"));
         $data = json_decode(file_get_contents("php://input"));
-        var_dump($data);
         $user = new \stdClass();
         $user->id = $id;
         $user->firstname = $data->firstname;
